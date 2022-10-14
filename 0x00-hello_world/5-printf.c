de76b3142ebcd879e4f910e14b13c1d9f6dfd25e
DevSan <sanusiolaniyan@gmail.com>
2022-10-13 17:57:15 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main function
+ * with the execution code 0 return value
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
