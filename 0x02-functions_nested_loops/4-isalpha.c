ca9d121624b9b1898268f7de0c82c1ffa54fc1b7
DevSan <sanusiolaniyan@gmail.com>
2022-10-18 13:27:51 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry function
+ * @c: the parameter to be passed
+ * Return: nothing
+ */
+int _isalpha(int c)
+{
+	if (c > 57)
+		return (1);
+	else
+		return (0);
+}
