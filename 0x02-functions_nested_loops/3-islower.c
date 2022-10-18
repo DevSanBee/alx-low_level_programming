fe3d50ed867235000a969677df3963f4b4ec1f52
DevSan <sanusiolaniyan@gmail.com>
2022-10-18 09:02:05 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ *
+ * Return: nothing
+ */
+
+int _islower(int c)
+{
+	if (c > 57)
+		_putchar('0');
+	_putchar('1');
+	_putchar('\n');
+	return (0);
+}
