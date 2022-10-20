28f67794744f3d1e57ed73362b57da2562dbd5f9
DevSan <sanusiolaniyan@gmail.com>
2022-10-20 15:01:51 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - function that draws a straight line i
+ * the terminal.
+ * @n: parameter to be passed
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (n <= 0)
+			_putchar('\n');
+		_putchar('_');
+	}
+}
