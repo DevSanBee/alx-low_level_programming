e7f5f70777de75b481c32fd80489f661d3ea9804
DevSan <sanusiolaniyan@gmail.com>
2022-10-20 13:35:37 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isdigit - function that check the parameter
+ * @c: the parameter to be passed
+ * Return: the parameter c
+ */
+int _isdigit(int c)
+{
+	return (c >= 0 && c <= 9);
+}
