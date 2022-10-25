78cecb7e2013d96dd2a239428b1eb2f9fa2e744a
DevSan <sanusiolaniyan@gmail.com>
2022-10-25 10:08:53 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - The function that checks the len
+ * @s: The parameter to be passed
+ * Return: nothing
+ */
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
