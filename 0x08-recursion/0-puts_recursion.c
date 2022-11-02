94dd6d1471fc7f3dd7997122fdbe107c9d1958ee
DevSan <sanusiolaniyan@gmail.com>
2022-11-02 14:14:53 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,8 +10,9 @@ void _puts_recursion(char *s)
 
 	if (*(s + i) != '\0')
 	{
-		_putchar(*(s + i));
+		_putchar(*s);
 		i++;
-		_puts_recursion(char *s);
+		_puts_recursion(s + i);
 	}
+	_putchar('\n');
 }
