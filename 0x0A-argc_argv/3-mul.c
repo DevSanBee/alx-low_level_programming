5972a02dd6dbfe27a9262dc5cd762c53cfe5f958
DevSan <sanusiolaniyan@gmail.com>
2022-11-04 13:38:13 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the actuall function
+ * @argc: the arguments count
+ * @argv: the arguments vector
+ * Return: 0 everry success
+ */
+int main(int argc, char *argv[])
+{
+	int sum;
+
+	if (argc < 2)
+		printf("Error\n");
+	sum = (atoi(argv[-1]) + atoi(argv[-2]));
+	printf("%d\n", sum);
+	return (0);
+}
