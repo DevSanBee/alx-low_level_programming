a5554faea971570683e12622170324d846cca78c
DevSan <sanusiolaniyan@gmail.com>
2022-11-04 12:37:07 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the function to print my name
+ * @argc: argument count
+ * @argv: argument vectors
+ * Return: Always 0 for success/correct
+ */
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
