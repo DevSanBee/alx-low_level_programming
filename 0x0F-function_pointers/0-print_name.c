b70508b6640c465a879291422216dd42b33e04ad
DevSan <sanusiolaniyan@gmail.com>
2022-11-17 12:19:07 -0800

    README.md

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * print_name - the function to be run the program
+ * @name: one of the parameters
+ * @f: the other parameter
+ * Retuen: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
