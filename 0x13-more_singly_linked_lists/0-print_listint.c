6781ce73a0e9193d41eca0564c140f0a44f6c718
DevSan <sanusiolaniyan@gmail.com>
2022-11-30 16:35:26 -0800

    0-print_listint.c new

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t odes = 0;
+	size_t nodes = 0;
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->next);
+		printf("%d\n", h->n);
 		h = h->next;
 		nnodes++;
 	}
-	return (odes);
+	return (nodes);
 }
