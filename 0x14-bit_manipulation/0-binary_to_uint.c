7e9be2c9a870765c1517f66703b342b3ab03bcf6
DevSan <sanusiolaniyan@gmail.com>
2022-12-02 14:23:53 -0800

    0-binary_to_uint.c minified

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * binary_to_uint - Entry Point
  * @b: const char
@@ -12,22 +11,17 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 		return (0);
-
 	while (b[i + 1])
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		i++;
 	}
-
 	while (i >= 0)
 	{
 		res += ((b[i] - '0') * base);
 		base *= 2;
 		i--;
 	}
-
-
 	return (res);
-
 }
